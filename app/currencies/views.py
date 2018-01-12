ba08abf4939ca0e7209f10529df3a7ae56935740
amarangonihengling <ahengling@azi.com.br>
2018-01-12 00:26:57 -0300

    Work In Progress - Implementacão do método de conversão - [TODO] recuperar a taxa de conversão

--- app/currencies/views.py
+++ app/currencies/views.py
@@ -34,7 +34,17 @@ Returns:
 """
 @require_http_methods(["GET"])
 def convert(request):
-    return JsonResponse({"name": "convert"})
+    from_coin, to_coin, value = getParamsFromRequest(request, ["from", "to", "value"])
+    if (not isRequestParamsListValid([from_coin, to_coin, value])):
+        return JsonResponse({"erro": "Os parâmetros from, to e value são obrigatórios."})
+
+    rate = getConversionRate()
+    return JsonResponse({
+        "from": from_coin,
+        "to": to_coin,
+        "originalValue": float(value),
+        "convertedValue": convertValue(value, rate)
+    })
 
 """views.convertAndDownload
 Método que realiza a conversão de um valor em uma determinada moeda para 
@@ -56,3 +66,55 @@ Returns:
 @require_http_methods(["GET"])
 def convertAndDownload(request):
     return JsonResponse({"name": "convertAndDownload"})
+
+"""getParamsFromRequest
+Método que recebe o objeto da requisição e uma lista de parâmetros e retorna 
+uma lista com o valor dos parâmetros.
+
+Args:
+    request: <HttpRequest>
+    paramsList: <Array<String>>
+    
+Returns:
+     <Array<String>> contendo o valor dos parâmetros
+"""
+def getParamsFromRequest(request, paramsList):
+    return [request.GET.get(param) for param in paramsList]
+
+"""TODO
+"""
+def getConversionRate():
+    return 2
+
+"""convertValue
+Método que recebe um valor e uma taxa de conversão e retorna o valor convertido. 
+
+Args:
+    value: <Float> valor a ser convertido
+    rate: <Float> taxa de conversão
+    
+Returns:
+     <Float> valor convertido
+"""
+def convertValue(value, rate):
+    return float(value) * rate
+
+"""isRequestParamsListValid
+Método que recebe uma lista de valores e verifica se todos os valores são 
+diferentes de null.
+
+Args:
+    paramsList: <Array<Object>> lista de valores
+    
+Returns:
+     <Boolean> True caso os valores sejam diferente de null, 
+     False caso contrário.
+"""
+def isRequestParamsListValid(paramsList):
+    isValid = True
+    for param in paramsList:
+        if not param:
+            isValid = False
+            break
+
+    return isValid
