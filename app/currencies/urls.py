65719e515452833501b0e005f7eceb946f00acfa
amarangonihengling <ahengling@azi.com.br>
2018-01-14 17:51:17 -0300

    Reestruturacao do projeto para deploy no heroku

--- app/currencies/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'currencies'
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('convert/', views.convert, name='convert'),
-    path('convertAndDownload/', views.convert_and_download, name='convert_and_download'),
-]
