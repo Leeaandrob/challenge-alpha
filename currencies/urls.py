15a9fde477709d5d1a214d86aa6aefda41617c62
amarangonihengling <ahengling@azi.com.br>
2018-01-15 13:19:50 -0300

    - Trocando a abordagem das views para class based views

--- currencies/urls.py
+++ currencies/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'currencies'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('convert/', views.convert, name='convert'),
-    path('convertAndDownload/', views.convert_and_download, name='convert_and_download'),
+    path('', views.IndexView.as_view(template_name='currencies/index.html'), name='index'),
+    path('convert/', views.ConvertView.as_view(), name='convert'),
+    path('convertAndDownload/', views.ConvertAndDownloadView.as_view(), name='convert_and_download'),
 ]
--- currencies/views.py
+++ currencies/views.py
@@ -1,24 +1,25 @@
-from django.shortcuts import render
-from django.http import JsonResponse, HttpResponse
-from django.views.decorators.http import require_http_methods
+from django.http import JsonResponse
+from django.views import View
+from django.views.generic import ListView
 
 from .helpers import CurrenciesHelper
 helper = CurrenciesHelper()
 
 
-@require_http_methods(['GET'])
-def index(request):
+class IndexView(ListView):
     """
-    Método responsável por retornar a listagem de moedas com as cotações atuais.
-    :return HttpResponse:
+    View responsável por retornar a listagem de moedas com as cotações atuais.
     """
-    context = {'currencies_list': helper.get_all_currencies_with_values()}
-    return render(request, 'currencies/index.html', context)
+    template_name = 'currencies/index.html'
+    context_object_name = 'currencies_list'
 
-@require_http_methods(['GET'])
-def convert(request):
+    def get_queryset(self):
+        return helper.get_all_currencies_with_values()
+
+
+class ConvertView(View):
     """
-    Método que realiza a conversão de um valor em uma determinada moeda para
+    Realiza a conversão de um valor em uma determinada moeda para
     outra moeda.
 
     Parâmetros obrigatórios da requisição:
@@ -29,26 +30,27 @@ def convert(request):
     :return JsonResponse: Json contendo a moeda de origem, a moeda de destino,
     o valor orignal e o valor convertido.
     """
-    from_currency, to_currency, value = helper.get_params_from_request(request, ['from', 'to', 'amount'])
-    if (not helper.assert_request_params([from_currency, to_currency, value])):
-        return JsonResponse(status=500, data={"erro": "Os parâmetros from, to e amount são obrigatórios."})
 
-    if not (helper.is_currency_supported(from_currency) and helper.is_currency_supported(to_currency)):
-        return JsonResponse(status=500, data={"erro": f"As moedas suportadas são {helper.supported_currencies}"})
+    def get(self, request):
+        from_currency, to_currency, value = helper.get_params_from_request(request, ['from', 'to', 'amount'])
+        if (not helper.assert_request_params([from_currency, to_currency, value])):
+            return JsonResponse(status=500, data={"erro": "Os parâmetros from, to e amount são obrigatórios."})
 
-    rate = helper.get_current_conversion_rate(from_currency, to_currency)
-    return JsonResponse({
-        "from": from_currency,
-        "to": to_currency,
-        "originalValue": float(value),
-        "convertedValue": helper.convert_value(value, rate),
-        "ratesLastUpdatedAt": helper.get_last_time_rates_were_updated()
-    })
+        if not (helper.is_currency_supported(from_currency) and helper.is_currency_supported(to_currency)):
+            return JsonResponse(status=500, data={"erro": f"As moedas suportadas são {helper.supported_currencies}"})
 
-@require_http_methods(['GET'])
-def convert_and_download(request):
+        rate = helper.get_current_conversion_rate(from_currency, to_currency)
+        return JsonResponse({
+            "from": from_currency,
+            "to": to_currency,
+            "originalValue": float(value),
+            "convertedValue": helper.convert_value(value, rate),
+            "ratesLastUpdatedAt": helper.get_last_time_rates_were_updated()
+        })
+
+class ConvertAndDownloadView(View):
     """
-    Método que realiza a conversão de um valor em uma determinada moeda para
+    Realiza a conversão de um valor em uma determinada moeda para
     outra moeda. As informações são retornadas em forma de arquivo.
 
     Parâmetros obrigatórios da requisição:
@@ -61,26 +63,30 @@ def convert_and_download(request):
     :return HttpResponse: reponse contendo o arquivo com as informações
     processadas.
     """
-    from_currency, to_currency, value, type = helper.get_params_from_request(request, ['from', 'to', 'amount', 'type'])
-    if (not helper.assert_request_params([from_currency, to_currency, value, type])):
-        return JsonResponse(status=500, data={"erro": "Os parâmetros from, to, amount e type são obrigatórios."})
 
-    if not (helper.is_currency_supported(from_currency) and helper.is_currency_supported(to_currency)):
-        return JsonResponse(status=500, data={"erro": f"As moedas suportadas são {helper.supported_currencies}"})
+    def get(self, request):
+        from_currency, to_currency, value, type = helper.get_params_from_request(request,
+                                                                                 ['from', 'to', 'amount', 'type'])
+        if (not helper.assert_request_params([from_currency, to_currency, value, type])):
+            return JsonResponse(status=500, data={"erro": "Os parâmetros from, to, amount e type são obrigatórios."})
 
-    if not (helper.is_file_format_supported(type)):
-        return JsonResponse(status=500, data={"erro": f"Os formatos de arquivo suportados são {helper.supported_file_formats}."})
+        if not (helper.is_currency_supported(from_currency) and helper.is_currency_supported(to_currency)):
+            return JsonResponse(status=500, data={"erro": f"As moedas suportadas são {helper.supported_currencies}"})
 
-    file_adapter = helper.get_file_response_adapter(type)
-    file_adapter.create_file()
+        if not (helper.is_file_format_supported(type)):
+            return JsonResponse(status=500, data={
+                "erro": f"Os formatos de arquivo suportados são {helper.supported_file_formats}."})
 
-    header = ['From currency', 'To currency', 'Original Value', 'Converted Value', 'Last time rates were updated']
-    file_adapter.write_header(header)
+        file_adapter = helper.get_file_response_adapter(type)
+        file_adapter.create_file()
 
-    rate = helper.get_current_conversion_rate(from_currency, to_currency)
-    converted_value = helper.convert_value(value, rate)
-    last_time_rates_were_updated = helper.get_last_time_rates_were_updated()
-    content = [from_currency, to_currency, float(value), converted_value, last_time_rates_were_updated]
-    file_adapter.write_content(content)
+        header = ['From currency', 'To currency', 'Original Value', 'Converted Value', 'Last time rates were updated']
+        file_adapter.write_header(header)
 
-    return file_adapter.get_response()
+        rate = helper.get_current_conversion_rate(from_currency, to_currency)
+        converted_value = helper.convert_value(value, rate)
+        last_time_rates_were_updated = helper.get_last_time_rates_were_updated()
+        content = [from_currency, to_currency, float(value), converted_value, last_time_rates_were_updated]
+        file_adapter.write_content(content)
+
+        return file_adapter.get_response()
