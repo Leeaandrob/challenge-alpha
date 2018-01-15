77329c356e4601d609e016cd3da1f82a136c5b45
amarangonihengling <ahengling@azi.com.br>
2018-01-15 13:22:10 -0300

    - Retirando o atributo template da view index, já que o mesmo é setado dentro da classe

--- currencies/urls.py
+++ currencies/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'currencies'
 urlpatterns = [
-    path('', views.IndexView.as_view(template_name='currencies/index.html'), name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     path('convert/', views.ConvertView.as_view(), name='convert'),
     path('convertAndDownload/', views.ConvertAndDownloadView.as_view(), name='convert_and_download'),
 ]
